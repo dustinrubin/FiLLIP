1565fc68419549d2b9bd8db52e743c7dc80e0d31
dustinrubin <dustin.rubin5050@gmail.com>
2018-10-25 18:23:25 -0400

    FixedCaseWhereSomeNumberCalledMoreThanOnce

--- hourglass/hourglassDustin.hs
+++ hourglass/hourglassDustin.hs
@@ -1,14 +1,14 @@
 main = interact $ show . solve . (map.map) read  . map words . lines
 
-smallnumber = -999
-
 hourglassSum ((n1:n2:n3:_):(_:n4:_:_):(n5:n6:n7:_):_) = n1 + n2 + n3 + n4 + n5 + n6 + n7
-hourglassSum _ = smallnumber
 
-solve ([]:xs) = smallnumber
-solve ([]) = smallnumber
-solve array = maximum $ [hourglassSum array,solve $ tail array,solve $ map tail array]
+solve ((x:y:[]):_) = smallnumber
+solve array = maximum $ [hourglassSum array,solveDown $ tail array,solve $ map tail array]
 
+solveDown (ys:xs:[]) = smallnumber
+solveDown array = maximum $ [hourglassSum array, solveDown $ tail array]
+
+smallnumber = -999
 
 testData = [[1,1,1,0,0,0],
             [0,1,0,0,0,0],
