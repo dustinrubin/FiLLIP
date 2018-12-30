622e96b7d8221a5c8cf6f8054639d5c0ee5a36b8
dustinrubin <dustin.rubin5050@gmail.com>
2018-12-29 21:01:38 -0500

    Max Array Sum Problem

---
+++ maxArraySum/maxArraySumDustin.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+def maxSubsetSum(list):
+    oneBackSolution = 0
+    twoBackSolution = 0
+    for index in range(len(list)):
+        currentValue = list[index]
+        if currentValue <= 0 or twoBackSolution + currentValue < oneBackSolution:
+            twoBackSolution = oneBackSolution
+        else:
+            ( twoBackSolution, oneBackSolution) =  ( oneBackSolution, twoBackSolution + currentValue)
+    return max(twoBackSolution, oneBackSolution)
+
+
+
+
+if __name__ == "__main__":
+    l =[ 3, 5, 8, 10, 4, 5]
+    l2 = [3, 5, -7, 8, 10]
+
+    print(maxSubsetSum(l))
+    print(maxSubsetSum(l2))
+    
